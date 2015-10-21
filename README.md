1aa16e5194270c088d8f90b16940e97485d48ea6
Hanna Rudakouskaya <hanna.rudakouskaya@gmail.com>
2015-10-21 23:23:54 +0300

    Analysis done. Codebook is the next step

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # GetData_PeerAssesment1
+
 Code for performing Course Project for Getting and Cleaning Data MOOC, Coursera
+
+## Purpose of this project
+
+Demonstrate your ability to collect, work with, and clean a data set
+
+## Goal
+
+Prepare tidy data that can be used for later analysis.
+
+## The data for the project
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+## The site where the data was obtained originally
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+### Submit
+1. A tidy data set as described below (as .txt):
+  You should create one R script called run_analysis.R that does the following:
+  1. Merges the training and the test sets to create one data set
+  2. Extracts only the measurements on the mean and standard deviation for each measurement
+  3. Uses descriptive activity names to name the activities in the data set
+  4. Appropriately labels the data set with descriptive variable names
+  5. From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject
+2. A link to a Github repository with your script for performing the analysis (containing run_analysis.R, CodeBook.md, README.md, original data set, tidy data set from steps 1-4, tidy data set from step 5)
+3. A code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md
+4. Include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.
+
---
+++ run_analysis.R
@@ -0,0 +1,92 @@
+
+require(data.table)
+require(dplyr)
+
+# Read the data set
+
+data.directory <- "data/"
+if (!dir.exists(data.directory)) {
+    dir.create(data.directory)
+}
+
+data.archive <- "getdata_projectfiles_UCI HAR Dataset.zip"
+data.archive.path <- paste0(data.directory, data.archive)
+if (!file.exists(data.archive.path)){
+    download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+                  destfile=data.archive.path, method="curl")
+}
+
+data.unzipped <- "UCI HAR Dataset"
+data.unzipped.path <- paste0(data.directory, data.unzipped)
+if (!dir.exists(data.unzipped.path)) {
+    unzip(data.unzipped.path, files = NULL, list = FALSE,
+          overwrite = FALSE)
+}
+
+# OK, data is ready in the proper folder
+
+# Merge the data set
+# Actually merging here is just concatencating train and test data sets
+
+# Person numbers are written under subject_XXX.txt
+person.number.train <- read.table(file = "./data/UCI HAR Dataset/train/subject_train.txt")
+person.number.test <- read.table(file = "./data/UCI HAR Dataset/test/subject_test.txt")
+person.number <- rbind(person.number.train, person.number.test)
+colnames(person.number) <- c("PERSON_NUMBER")
+
+remove(person.number.train, person.number.test)
+head(person.number)
+
+# Activity numbers are written under y_XXX.txt
+activity.number.train <- read.table(file = "./data/UCI HAR Dataset/train/y_train.txt")
+activity.number.test <- read.table(file = "./data/UCI HAR Dataset/test/y_test.txt")
+activity.number <- rbind(activity.number.train, activity.number.test)
+colnames(activity.number) <- c("ACTIVITY_NUMBER")
+
+remove(activity.number.train, activity.number.test)
+
+# We have activities as numbers - need to have them as description
+
+# First let's have a table with activity labels
+activity.labels <- read.table(file = "./data/UCI HAR Dataset/activity_labels.txt")
+
+# So we will match the activity number with activity name
+activity.name <- sapply(activity.number, function (x) { factor(x,
+                                                               levels = activity.labels[,1],
+                                                               labels = activity.labels[,2]) })
+colnames(activity.name) <- c("ACTIVITY_NAME")
+
+remove(activity.labels, activity.number)
+head(activity.name)
+
+# Next we need to extract the features names
+features.names <- read.table("data/UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)[, 2]
+
+# Measures are written under X_XXX.txt
+measures.train <- read.table(file = "./data/UCI HAR Dataset/train/X_train.txt")
+measures.test <- read.table(file = "./data/UCI HAR Dataset/test/X_test.txt")
+measures <- rbind(measures.train, measures.test)
+colnames(measures) <- features.names
+
+remove(measures.train, measures.test)
+
+# Finally we can merge our data into one tidy data.table
+tidy.measures <- data.table(person.number,
+                            activity.name,
+                            measures)
+
+head(tidy.measures)
+colnames(tidy.measures)
+
+grep("std", colnames(tidy.measures))
+
+# OK, the next step is to extract only the measurements on the mean and standard deviation for each measurement
+tidy.mean.sd <- select(tidy.measures,
+                       PERSON_NUMBER,
+                       ACTIVITY_NAME,
+                       grep("mean", colnames(tidy.measures)),
+                       grep("std", colnames(tidy.measures)))
+
+# And finally create tidy data set with the average of each variable for each activity and each subject
+melted.tidy <- melt(data = tidy.mean.sd, id.vars = c("PERSON_NUMBER", "ACTIVITY_NAME"))
+tidy.final <- dcast(melted.tidy, PERSON_NUMBER + ACTIVITY_NAME ~ variable, mean)
